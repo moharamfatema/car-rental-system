24d32cae4322d8a34be3b344dd4d4e51171146bd
Nada Ali Nour Eldeen <48387157+NadaAlinour@users.noreply.github.com>
2022-05-25 11:13:40 +0300

    Update reports_DML.sql

--- server-side/reports_DML.sql
+++ server-side/reports_DML.sql
@@ -4,7 +4,7 @@ information*/
 
 SELECT R.plate_number, C.model, C.brand, C.year, C.status, R.customer_id, CU.fname, CU.lname, CU.email, Cu.phone_number, CU.password, R.pickup_date, R.reservation_id, R.pickup_office, R.return_office, R.booking_status, R.payment_status
 FROM reservation AS R JOIN car AS C ON R.plate_number = C.plate_number JOIN customer AS CU ON R.customer_id = CU.customer_id
-WHERE R.pickup_date BETWEEN '2022-01-01' AND '2022-12-01';
+WHERE R.pickup_date BETWEEN 'from_date_input' AND 'to_date_input';
 
 /*all reservations of any car within a specified period including all car
 information*/
