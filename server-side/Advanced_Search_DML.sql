19d6180803834213e1a2aceefe1f2ec16b18b910
abdelwahabmira <35869375+abdelwahabmira@users.noreply.github.com>
2022-05-25 20:50:55 +0200

    Update Advanced_Search_DML.sql

--- server-side/Advanced_Search_DML.sql
+++ server-side/Advanced_Search_DML.sql
@@ -2,6 +2,10 @@ SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(custom
 FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
 WHERE car.plate_number='Plate_Num'
 
+SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(customer.fname,customer.lname) , reservation.pickup_date ,reservation.reservation_id 
+FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
+WHERE car.brand='brand'
+
 
 SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(customer.fname,customer.lname) , reservation.pickup_date ,reservation.reservation_id 
 FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
@@ -12,8 +16,22 @@ SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(custom
 FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
 WHERE customer.fname='Fname' OR customer.lname='Lname'
 
+SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(customer.fname,customer.lname) , reservation.pickup_date ,reservation.reservation_id 
+FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
+WHERE customer.customer_id='Id'
+
+SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(customer.fname,customer.lname) , reservation.pickup_date ,reservation.reservation_id 
+FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
+WHERE customer.phone_number='Phone_Num'
+
+
+
 
 SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(customer.fname,customer.lname) , reservation.pickup_date ,reservation.reservation_id 
 FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
 WHERE reservation.pickup_date='Date'
 
+
+SELECT car.plate_number , car.model , customer.customer_id , GROUP_CONCAT(customer.fname,customer.lname) , reservation.pickup_date ,reservation.reservation_id 
+FROM ((reservation INNER JOIN car ON reservation.plate_number=car.plate_number)INNER JOIN customer ON reservation.customer_id=customer.customer_id) 
+WHERE reservation.reservation_id='reserv_id'
